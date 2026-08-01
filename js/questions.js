3af83b1ace4235c871aa7b321cfaead391c494e0
thidiemcao <thayhoang.contact@gmail.com>
2026-08-01 16:43:57 +0700

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -1,1 +1,48 @@
-const questions = [];
+const questions = [
+
+{
+question:"1 + 1 bằng bao nhiêu?",
+A:"1",
+B:"2",
+C:"3",
+D:"4",
+correct:"B"
+},
+
+{
+question:"2 × 5 bằng bao nhiêu?",
+A:"7",
+B:"8",
+C:"10",
+D:"12",
+correct:"C"
+},
+
+{
+question:"9 − 3 bằng bao nhiêu?",
+A:"6",
+B:"5",
+C:"4",
+D:"3",
+correct:"A"
+},
+
+{
+question:"Căn bậc hai của 81 là?",
+A:"7",
+B:"8",
+C:"9",
+D:"10",
+correct:"C"
+},
+
+{
+question:"Giá trị của π gần đúng là?",
+A:"2.14",
+B:"3.14",
+C:"4.13",
+D:"5.14",
+correct:"B"
+}
+
+];
